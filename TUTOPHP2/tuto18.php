598e09ef1a0e540cda6dbc1fdab8430a108fd81d
carlos diaz <carloseduardodiaz0316@gmail.com>
2025-05-02 17:45:21 -0500

    inicio tutorial 2

---
+++ TUTOPHP2/tuto18.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="tuto18_proceso.php" method="post">
+        <label for="">Nombre</label><br>
+        <input type="text" name="nombre"><br>
+        <label for="">contraseña</label><br>
+        <input type="password" name="contraseña"><br>
+        <label for="">confirmar contraseña</label><br>
+        <input type="password" name="contraseña2">
+        <br>
+        <br>
+        <input type="submit" value="registrarse">
+    </form>
+
+</body>
+</html>
